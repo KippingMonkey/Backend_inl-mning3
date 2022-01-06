7322262e9f942f3cdde74bd7edda8f3ae341c883
KippingMonkey <73753894+KippingMonkey@users.noreply.github.com>
2022-01-06 09:31:50 +0100

    update to current, minor changes

--- Musik-Affär/Musik-Affär/Pages/Reviews/Create.cshtml.cs
+++ Musik-Affär/Musik-Affär/Pages/Reviews/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,11 +14,14 @@ namespace Musik_Affär.Pages.Reviews
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public CreateModel(ApplicationDbContext context)
+        public CreateModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
         [BindProperty]
         public Product Product { get; set; }
         public double NewScore { get; set; }
@@ -25,18 +29,19 @@ namespace Musik_Affär.Pages.Reviews
         [BindProperty]
         public Review Review { get; set; }
 
-        public IActionResult OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
-            //Product = _context.Products.FirstOrDefault(m => m.ID == id);
+            
+            Product =  _context.Products.FirstOrDefault(m => m.ID == id);
+            IdentityUser user = await _userManager.GetUserAsync(User);
 
             ////NewScore = _context.Reviews.Where(r => r.ID == id).Average(g => g.Grade);
             //NewScore = 3.4;
 
-            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            //ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
             return Page();
         }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync(int id, Review review)
         {
             if (!ModelState.IsValid)
@@ -44,9 +49,9 @@ namespace Musik_Affär.Pages.Reviews
                 return Page();
             }
 
-            //Review.Grade = review.Grade;
-            //Review.UserID =;
-            //Review.Product = Product;
+            Review.Grade = review.Grade;
+            Review.UserID = review.UserID;
+            Review.Product.ID = review.Product.ID;
             //Product.Score = NewScore;
             //Test för att ändra score i products men det funkar inte för mig
             //_context.Products.Attach(Product).Property(p => p.Score).IsModified = true;
