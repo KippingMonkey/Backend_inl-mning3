7223c5d9644b5145c7c317d55f73c48d08724eba
KippingMonkey <73753894+KippingMonkey@users.noreply.github.com>
2022-01-18 10:05:04 +0100

    add change grade function to reviewsIndex page

--- Musik-Affär/Musik-Affär/Pages/Reviews/Index.cshtml.cs
+++ Musik-Affär/Musik-Affär/Pages/Reviews/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,19 +13,41 @@ namespace Musik_Affär.Pages.Reviews
 {
     public class IndexModel : PageModel
     {
-        private readonly Musik_Affär.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(Musik_Affär.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<Review> Review { get;set; }
+        public IList<Review> Reviews { get;set; }
+
+        [BindProperty]
+        public Review Review { get;set; }
+        [BindProperty]
+        public byte Grade { get;set; }
 
         public async Task OnGetAsync()
         {
-            Review = await _context.Reviews
-                .Include(r => r.User).ToListAsync();
+            IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            Reviews = await _context.Reviews.Include(r => r.User)
+                                            .Include(r => r.Product)
+                                            .Where(r => r.User == user)
+                                            .ToListAsync();
+          
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id, byte grade)
+        {
+            Review = await _context.Reviews.Where(r => r.ID == id).FirstOrDefaultAsync();
+
+            Review.Grade = grade;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
