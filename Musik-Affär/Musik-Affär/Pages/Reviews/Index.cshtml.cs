a8943534f32c46d0b316c689f2c48d92fa593594
KippingMonkey <73753894+KippingMonkey@users.noreply.github.com>
2022-01-20 10:38:38 +0100

    add search, filter and sort to reviewIndex page. add search-reset button to search-forms on product, cart and reviews Index pages

--- Musik-Affär/Musik-Affär/Pages/Reviews/Index.cshtml.cs
+++ Musik-Affär/Musik-Affär/Pages/Reviews/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Musik_Affär.Data;
 using Musik_Affär.Models;
@@ -32,6 +33,37 @@ namespace Musik_Affär.Pages.Reviews
         public double NewScore { get;set; }
         public Product Product { get;set; }
 
+        [FromQuery]
+        public string SearchTerm { get; set; }
+
+        private const string nameColumn = "Namn";
+        private const string categoryColumn = "Kategori";
+        private const string colorColumn = "Färg";
+        private const string priceColumn = "Pris";
+        private const string brandColumn = "Märke";
+        private const string scoreColumn = "Betyg";
+
+        private const string ascendingColumn = "Stigande";
+        private const string descendingColumn = "Fallande";
+
+        private string[] sortColumns = { nameColumn, categoryColumn, colorColumn, priceColumn, brandColumn, scoreColumn };
+        private string[] directions = { ascendingColumn, descendingColumn };
+
+        [FromQuery]
+        public string SortColumn { get; set; }
+        [FromQuery]
+        public string Direction { get; set; }
+        [FromQuery]
+        public string Brand { get; set; }
+        [FromQuery]
+        public string Category { get; set; }
+        [FromQuery]
+        public string Color { get; set; }
+
+        public SelectList SortColumnList { get; set; }
+        public SelectList DirectionList { get; set; }
+
+
         public async Task OnGetAsync()
         {
             IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
@@ -40,7 +72,56 @@ namespace Musik_Affär.Pages.Reviews
                                             .Include(r => r.Product)
                                             .Where(r => r.User == user)
                                             .ToListAsync();
-          
+
+            var query = Reviews;
+
+            if (Color != null) query = query.Where(q => q.Product.Color == Enum.GetName(typeof(Product.Style), int.Parse(Color))).ToList();
+            if (Category != null) query = query.Where(q => q.Product.Category == Enum.GetName(typeof(Product.Type), int.Parse(Category))).ToList();
+            if (Brand != null) query = query.Where(q => q.Product.Brand == Enum.GetName(typeof(Product.Manufacturer), int.Parse(Brand))).ToList();
+
+
+            SortColumnList = new SelectList(sortColumns);
+            DirectionList = new SelectList(directions);
+
+            if (SearchTerm != null)
+            {
+                query = query.Where(c =>
+                    c.Product.Name.ToLower().Contains(SearchTerm.ToLower()) ||
+                    c.Product.Category.ToLower().Contains(SearchTerm.ToLower()) ||
+                    c.Product.Color.ToLower().Contains(SearchTerm.ToLower()) ||
+                    c.Product.Brand.ToLower().Contains(SearchTerm.ToLower())
+                ).ToList();
+            }
+
+            if (SortColumn != null)
+            {
+                if (SortColumn == nameColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Name).ToList() : query.OrderBy(c => c.Product.Name).ToList();
+                }
+                else if (SortColumn == categoryColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Category).ToList() : query.OrderBy(c => c.Product.Category).ToList();
+                }
+                else if (SortColumn == colorColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Color).ToList() : query.OrderBy(c => c.Product.Color).ToList();
+                }
+                else if (SortColumn == brandColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Brand).ToList() : query.OrderBy(c => c.Product.Brand).ToList();
+                }
+                else if (SortColumn == priceColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Price).ToList() : query.OrderBy(c => c.Product.Price).ToList();
+                }
+                else if (SortColumn == scoreColumn)
+                {
+                    query = (Direction == "Fallande") ? query.OrderByDescending(c => c.Product.Score).ToList() : query.OrderBy(c => c.Product.Score).ToList();
+                }
+            }
+            Reviews = query;
+
         }
 
         public async Task<IActionResult> OnPostChange(int id, byte grade)
